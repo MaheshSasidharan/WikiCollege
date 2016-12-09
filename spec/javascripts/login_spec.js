a77c3c8844f97eb3624a33fdbe64cdcae3f32264
Kaustubh <kaustubh-mungale@uiowa.edu>
2016-12-09 08:26:34 +0000

    more specs

--- spec/javascripts/login_spec.js
+++ spec/javascripts/login_spec.js
@@ -2,10 +2,16 @@ describe('Wiki college', function() {
   beforeEach(module('myapplication'));
 
   var $controller;
+  var $q;
+  var $rootScope;
+  var $httpBackend;
 
-  beforeEach(inject(function(_$controller_){
+  beforeEach(inject(function(_$controller_, _$q_, _$rootScope_, _$httpBackend_){
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $controller = _$controller_;
+    $q = _$q_;
+    $rootScope = _$rootScope_;
+    $httpBackend = _$httpBackend_;
   }));
 
   describe('Login Page Controller', function() {
@@ -30,6 +36,17 @@ describe('Wiki college', function() {
           username: 'abcd@wikicollege.com'
       }); 
       expect('strong').toEqual('strong');
+      
+      var deferred = $q.defer();
+      var promise = deferred.promise;
+      var response
+      deferred.resolve(123);
+      deferred.promise.then(function(data) {
+        response = data;
+      });
+      $httpBackend.whenPOST("/users/LoginUser").respond({ hello: 'World' });
+      $rootScope.$apply();
+      //spyOn(controller.Helper, 'LoginUser').and.returnValue(deferred.promise);
     })
     
   });
