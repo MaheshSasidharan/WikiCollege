6e35010a81092208797833c264411f6be4842cbe
Kaustubh <kaustubh-mungale@uiowa.edu>
2016-12-09 01:33:45 +0000

    Adding jasmine test cases for home and Uni details

---
+++ spec/javascripts/home_spec.js
@@ -0,0 +1,51 @@
+describe('Wiki college', function() {
+  beforeEach(module('myapplication'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  describe('Home Page controller', function() {
+    it('Should return user info', function() {
+      var $scope = {};
+      var controller = $controller('HomeCtrl', { $scope: $scope });
+      controller.oLoginItem = {
+          Logout: function(){}
+      }
+      controller.ShowHideLogin();
+      //expect('strong').toEqual('strong');
+    });
+    
+    it('should show login item if already logged', function() {
+      var $scope = {};
+      var controller = $controller('HomeCtrl', { $scope: $scope });
+      controller.oLoginItem = {
+          sharedProperties: {},
+          logout: function(){},
+          bShow: undefined
+      }
+      controller.ShowHideLogin();
+      //controller.oService.GetUserInfo();
+
+      //expect('strong').toEqual('strong');
+    });
+    
+    it('should logout if user is logged in', function() {
+      var $scope = {};
+      var controller = $controller('HomeCtrl', { $scope: $scope });
+      controller.oLoginItem = {
+          sharedProperties: {},
+          logout: function(){},
+          bShow: undefined,
+          bLoggedIn: true
+      }
+      controller.ShowHideLogin();
+      //controller.oService.GetUserInfo();
+
+      //expect('strong').toEqual('strong');
+    });
+  });
+});
