44b22bd13fd84cfefb7505507ff6ecc63975b46e
Raghavendranath <raghavendranath-kandula@uiowa.edu>
2016-12-09 11:26:05 +0000

    R

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,7 @@ class Comment < ActiveRecord::Base
     #has_many :likecomments
     
     
-    validates :commentData, presence: true, length: {minimum: 3, maximum: 200}
+    #validates :commentData, presence: true, length: {minimum: 3, maximum: 200}
     
     # def self.updateCommentCommentId(commentId, text)
     #     where("user = ? AND post = ?", "%#{userId}%", "%#{postId}%")
